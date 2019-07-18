17ea71dc60b7d7c58bb2f3bff020f1521b3e0970
Joseph Carusone <jcarusone@myseneca.ca>
2019-07-18 00:15:44 -0400

    First Commit

---
+++ src/build/precache-manifest.2e600ea4acfc247dce633fc818ef9358.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea9d97441093b8c7cbac640fac555b8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50e9c3ec2d968a8d768c",
+    "url": "/static/css/main.592668b7.chunk.css"
+  },
+  {
+    "revision": "29f2dd377d2ab554ba3e",
+    "url": "/static/js/2.01c54b2c.chunk.js"
+  },
+  {
+    "revision": "50e9c3ec2d968a8d768c",
+    "url": "/static/js/main.fd3c195a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
